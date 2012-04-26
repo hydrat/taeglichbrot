45d8205200d1054dcc335cf4fc8b24ed8409e9d6
Christian Rohrer <hydrat@gmail.com>
2012-04-27 01:15:51 +0200

    new Rails app generated by Rails Apps Composer gem

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Taeglichbrot::Application.config.secret_token = '90cd684699387a53f7974b5e83e9d1aaa44600106e11f29ca09be9799e738e0233be55fa2b64bc1d504c0e05689db231c32479ed366a6cb60650e454e73b128c'
